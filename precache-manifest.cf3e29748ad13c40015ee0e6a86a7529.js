b56bb06d57e5c5d0f8479467778fd4d5a6fb0023
Maciej Biel <maciek.b2k@gmail.com>
2020-12-30 14:24:04 +0100

    Updates

---
+++ precache-manifest.cf3e29748ad13c40015ee0e6a86a7529.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b30c9596369661c0e8a00af95360968",
+    "url": "/square_breathing/index.html"
+  },
+  {
+    "revision": "649d49b4718e17ed974c",
+    "url": "/square_breathing/static/css/2.6fdab21f.chunk.css"
+  },
+  {
+    "revision": "32804e85859cbcc5d430",
+    "url": "/square_breathing/static/css/main.5f9d7621.chunk.css"
+  },
+  {
+    "revision": "649d49b4718e17ed974c",
+    "url": "/square_breathing/static/js/2.2fe9807a.chunk.js"
+  },
+  {
+    "revision": "7c11ec4032322c0b2845556f0c0f68bd",
+    "url": "/square_breathing/static/js/2.2fe9807a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb2f33558adf23a672b",
+    "url": "/square_breathing/static/js/3.22a33ee2.chunk.js"
+  },
+  {
+    "revision": "32804e85859cbcc5d430",
+    "url": "/square_breathing/static/js/main.c82a0757.chunk.js"
+  },
+  {
+    "revision": "24b0cf693f94c3de5cc8",
+    "url": "/square_breathing/static/js/runtime-main.2519c600.js"
+  }
+]);
